6057e9ebba53f7388172a4824acdfeb1b0d7636a
Jack Radinger <jackradinger98@gmail.com>
2021-02-04 13:14:47 -0600

    changes

--- storage-project.js
+++ storage-project.js
@@ -6,10 +6,26 @@ window.addEventListener("DOMContentLoaded", (event) => {
   const select = document.getElementById("items");
   console.log(select);
   console.log(select.value);
-  const form = document.querySelector(".form form__container");
+  const input = document.getElementById('')
+  // const quality = document.
+
+  const form = document.querySelector("form ");
+  const appleValue = localStorage.getItem('apple');
+  // const bananaValue = localStorage.getItem('Banana');
+  // const appleValue = localStorage.getItem('Cookie');
+  // const appleValue = localStorage.getItem('Donut');
+  // const appleValue = localStorage.getItem('Eggs');
+  // const appleValue = localStorage.getItem('Falafel');
+  // const appleValue = localStorage.getItem('Granola Bar');
+  // const appleValue = localStorage.getItem('Hot Dog');
+  // const appleValue = localStorage.getItem('Ice Cream Pint');
+  // const appleValue = localStorage.getItem('Jalepeno');
+
+  console.log(form);
   form.addEventListener("submit", (event) => {
     event.preventDefault();
     localStorage.setItems(`${select.value}`, select.value);
+
   });
   const storeItem = () => {
 
